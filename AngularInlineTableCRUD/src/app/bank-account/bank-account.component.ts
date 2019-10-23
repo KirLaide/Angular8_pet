0ddfc77e0c68179f94e7461a46388fd1b8acec21
Дисненко Юрий Владимирович <Yury.Disnenko@norbit.ru>
2019-10-23 16:41:53 +0300

    crude update, delete, insert

--- AngularInlineTableCRUD/src/app/bank-account/bank-account.component.ts
+++ AngularInlineTableCRUD/src/app/bank-account/bank-account.component.ts
@@ -12,6 +12,7 @@ export class BankAccountComponent implements OnInit {
 
   bankAccountForms : FormArray = this.fb.array([]);
   bankList = [];
+  notification = null;
 
   constructor(private fb: FormBuilder,
      private bankService: BankService,
@@ -22,7 +23,25 @@ export class BankAccountComponent implements OnInit {
       .subscribe(res => this.bankList = res as []);
 
 
-    this.addBankAccountForm();
+    
+
+    this.service.getBankAccountList().subscribe(
+        res =>{
+          if(res == [])
+          this.addBankAccountForm();
+          else {
+            (res as []).forEach((bankAccount : any ) => {
+              this.bankAccountForms.push(this.fb.group({
+                bankAccountID : [bankAccount.bankAccountID],
+                accountNumber : [bankAccount.accountNumber, Validators.required],
+                accountHolder : [bankAccount.accountHolder, Validators.required],
+                bankID : [bankAccount.bankID, Validators.min(1)],
+                IFSC : [bankAccount.ifsc,Validators.required]
+              }));
+            }); 
+          }
+        }
+    );
   }
 
   addBankAccountForm(){
@@ -36,10 +55,53 @@ export class BankAccountComponent implements OnInit {
   }
 
   recordSubmit(fg:FormGroup){
-    var temp = fg.value;
-    this.service.postBankAccount(temp)
-    .subscribe(
-      (res: any) => {}
-    )
+      if (fg.value.bankAccountID == 0)
+      this.service.postBankAccount(fg.value)
+      .subscribe(
+        (res: any) => {
+          fg.patchValue({ bankAccountID: res.bankAccountID });
+          this.showNotification('insert');
+        }
+      );
+    else{
+      this.service.putBankAccount(fg.value)
+      .subscribe(
+        (res: any) => {
+          this.showNotification('update');
+        }
+      );
+    }  
+  }
+
+  onDelete(bankAccountID,i){
+    if(bankAccountID == 0)
+      this.bankAccountForms.removeAt(i);
+    else if (confirm('Точно удалить?')){
+      this.service.deleteBankAccount(bankAccountID).subscribe(
+          res => {
+            this.bankAccountForms.removeAt(i);
+            this.showNotification('delete');
+          }
+      );
+    }
+  }
+
+  showNotification(category) {
+    switch (category) {
+      case 'insert':
+        this.notification = {class:'text-success', message:'saved!'}
+        break;
+       case 'update':
+          this.notification = {class:'text-primary', message:'updated!'}
+          break;
+      case 'delete':
+        this.notification = {class:'text-danger', message:'deleted!'}
+        break;
+    
+      default:
+        break;
+    }
+
+
   }
 }
